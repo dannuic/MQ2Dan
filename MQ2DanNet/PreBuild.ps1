93cce116611bbbbe6a8909981196ff87da149014
Knight Lee <55611098+Knightly1@users.noreply.github.com>
2021-01-04 06:34:10 -1000

    Update vcpkg method (#50)

--- MQ2DanNet/PreBuild.ps1
+++
@@ -1,6 +0,0 @@
-param([string]$VCPkgRoot);
-Set-Location $VCPkgRoot
-if (!(Test-Path ".\vcpkg.exe")) {
-    & ".\bootstrap-vcpkg.bat"
-}
-& ".\vcpkg.exe" install czmq[draft]:x86-windows-static zyre:x86-windows-static
